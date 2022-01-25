57bec92d77822f6d5f5974c2b612dbe11ae164e0
Lu Nguyen <lu@redweb.dk>
2022-01-25 22:02:16 +0700

    Price

--- src/model/RadaBidModel.js
+++ src/model/RadaBidModel.js
@@ -1,12 +1,15 @@
-import { formatEther, formatUnits } from "@ethersproject/units";
+import { formatUnits } from "@ethersproject/units";
 
 const RadaBidModel = (data, poolId, id) => {
   console.log("RadaStatsModel", data);
+
+  const addressPayableDecimals = localStorage.getItem("addressPayableDecimals");
+
   return {
     id: id ?? "0",
     poolId: poolId ?? "0",
     creator: data.creator ?? "",
-    priceEach: data.priceEach ? formatEther(data.priceEach) : "0",
+    priceEach: data.priceEach ? formatUnits(data.priceEach, addressPayableDecimals) : "0",
     quantity: data.quantity ? formatUnits(data.quantity, 0) : "0",
     winQuantity: data.winQuantity ? formatUnits(data.winQuantity, 0) : "0",
     claimed: data.claimed ?? false,
